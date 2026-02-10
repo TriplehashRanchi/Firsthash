ee52d44b7a5eabb03348902136b046d127ba97e9
TriplehashRanchi <168893404+TriplehashRanchi@users.noreply.github.com>
2026-02-10 10:48:20 +0530

    logo width

--- components/layouts/adminSidebar.tsx
+++ components/layouts/adminSidebar.tsx
@@ -103,7 +103,7 @@ const AdminSidebar = () => {
                 <div className="h-full bg-white dark:bg-black">
                     <div className="flex items-center justify-between px-4 py-3">
                         <Link href="/" className="main-logo flex shrink-0 items-center">
-                            <img className="ml-[5px] w-8 flex-none" src="/assets/images/logo.png" alt="logo" />
+                            <img className="ml-[5px] w-40 flex-none" src="/assets/images/logo.png" alt="logo" />
                             {/* <span className="align-middle text-2xl font-semibold ltr:ml-1.5 rtl:mr-1.5 dark:text-white-light lg:inline">FIRSTHASH</span> */}
                         </Link>
 
